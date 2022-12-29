8916cfbfa4555b55bed38b918a010178fd309c10
Manasraj34533 <krish2102050@gmail.com>
2022-12-29 16:34:01 +0530

    message

--- my-app/pages/Models/BaseUrl.jsx
+++ my-app/pages/Models/BaseUrl.jsx
@@ -1,6 +1,3 @@
-const baseUrl = process.env.NODE_ENV === "production" ? "https://mystore0023.vercel.app" : "http://localhost:3000/"
-<<<<<<< HEAD
+const baseUrl = process.env.NODE_ENV === "production" ? "https://.vercel.app" : "http://localhost:3000/"
+
 export default baseUrl;
-=======
-export default baseUrl;
->>>>>>> dccfbb85d6ace01bcbdc95d2b0d2b86f92e3f9bf
